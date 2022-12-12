8166cd00a18ee07af6ab5db1ce4ad15688d7f714
Zhiying Xie <zhiyix2@uw.edu>
2022-12-12 03:34:36 -0800

    Update built files and README.md

--- src/Draw_Reaction/__init__.py
+++ src/Draw_Reaction/__init__.py
@@ -7,7 +7,7 @@ except ModuleNotFoundError:
     import tomli as tomllib
 
 # Version of the Draw_Reaction package
-__version__ = "0.0.2"
+__version__ = "0.0.3"
 
 _cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
 URL = _cfg["feed"]["url"]

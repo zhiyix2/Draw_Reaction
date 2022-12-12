d1c16760b4a807f3136f7c30f3948db724bea870
Zhiying Xie <zhiyix2@uw.edu>
2022-12-11 18:03:29 -0800

    Add __main__.py and __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# __init__.py
+
+from importlib import resources
+try:
+    import tkinter
+except ModuleNotFoundError:
+    import tomli as tomllib
+
+# Version of the Draw_Reaction package
+__version__ = "0.0.2"
+
+_cfg = tomllib.loads(resources.read_text("reader", "config.toml"))
+URL = _cfg["feed"]["url"]
--- src/Draw_Reaction/Draw_Reaction.py
+++ src/Draw_Reaction/Draw_Reaction.py
@@ -66,48 +66,4 @@ class Draw_Reaction(Frame):
         
 
         canvas.pack(fill=BOTH, expand=1)
-# Determine the Maximum Column Number
-# Input: Reaction_List
-# Output: Max as Int
-def getMaxColumn(reaction_list):
-    max = 0
-    for reaction in reaction_list:
-        if len(reaction) > max:
-            max = len(reaction)
-    return max
-    
-# Main Method
-def main(values):
-    #values = sys.argv[1:]
 
-    try:
-        root = Tk()
-        
-        ex = Draw_Reaction(values)
-        margin = 10
-        height = 40
-        width = 100
-        reaction_list = Read_Compound_Reaction.get_reaction_list(Read_Compound_Reaction.readFile(values[0]))
-
-        inputList = Read_Compound_Reaction.get_reaction_data(reaction_list)
-        inputList = Read_Compound_Reaction.get_compound_reaction(inputList)
-
-        
-        rows = len(inputList)
-        column = getMaxColumn(inputList)
-        windowHeight = (rows * margin + 2) + (height * rows + 2)
-        windowWidth = (column * margin + 2) + (column * width + 2)
-            
-        height_width = "{}x{}+300+300".format(windowWidth, windowHeight)
-        #print(height_width)
-        root.geometry(height_width)
-        root.mainloop()
-    except getopt.error as err:
-        # output error, and return with an error code
-        print(str(err))
-        print("Incorrect File Names given <input file>")
-
-
-
-if __name__ == '__main__':
-    main(sys.argv[1:])
